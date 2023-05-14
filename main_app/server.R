0eb0e89af2077461ef780f0424b0eb071b8a52a4
maciej <macieklorens@gmail.com>
2023-05-14 14:42:43 +0200

    server i ui oddzielnie

---
+++ main_app/server.R
@@ -0,0 +1,38 @@
+library(ggplot2)
+
+# Define server logic to read selected file ----
+function(input, output) {
+  
+  output$contents <- DT::renderDataTable(DT::datatable({
+    
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, head of that data file by default,
+    # or all rows if selected, will be shown.
+    
+    req(input$file1)
+    
+    # when reading semicolon separated files,
+    # having a comma separator causes `read.csv` to error
+    tryCatch(
+      {
+        df <- read.csv(input$file1$datapath,
+                       header = input$header,
+                       sep = input$sep,
+                       quote = input$quote)
+      },
+      error = function(e) {
+        # return a safeError if a parsing error occurs
+        stop(safeError(e))
+      }
+    )
+    
+    if(input$disp == "head") {
+      return(head(df))
+    }
+    else {
+      return(df)
+    }
+    
+  }), options = list(scrollX = TRUE))
+  
+}
---
+++ main_app/ui.R
@@ -0,0 +1,63 @@
+library(ggplot2)
+
+navbarPage(
+  "EasyNet",
+  # App title ----
+  
+  tabPanel("Uploading files",
+    
+  # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+      
+      # Sidebar panel for inputs ----
+      sidebarPanel(
+        
+        # Input: Select a file ----
+        fileInput("file1", "Choose CSV File",
+                  multiple = FALSE,
+                  accept = c("text/csv",
+                             "text/comma-separated-values,text/plain",
+                             ".csv")),
+        
+        # Horizontal line ----
+        tags$hr(),
+        
+        # Input: Checkbox if file has header ----
+        checkboxInput("header", "Header", TRUE),
+        
+        # Input: Select separator ----
+        radioButtons("sep", "Separator",
+                     choices = c(Comma = ",",
+                                 Semicolon = ";",
+                                 Tab = "\t"),
+                     selected = ","),
+        
+        # Input: Select quotes ----
+        radioButtons("quote", "Quote",
+                     choices = c(None = "",
+                                 "Double Quote" = '"',
+                                 "Single Quote" = "'"),
+                     selected = '"'),
+        
+        # Horizontal line ----
+        tags$hr(),
+        
+        # Input: Select number of rows to display ----
+        radioButtons("disp", "Display",
+                     choices = c(Head = "head",
+                                 All = "all"),
+                     selected = "head")
+        
+      ),
+      
+      # Main panel for displaying outputs ----
+      mainPanel(
+        
+        # Output: Data file ----
+        DT::dataTableOutput("contents")
+        
+      )
+      
+    )
+  )
+)
