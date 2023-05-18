1ab5cb74753cee5c7cea56ac54c4628a5bfd2cd6
AlexWieli <128088524+AlexWieli@users.noreply.github.com>
2023-05-18 22:53:02 +0200

    Add files via upload
    tutaj wersje z przyciskiem undo, żeby można było cofać zmiany

---
+++ ui_Olek.R
@@ -0,0 +1,99 @@
+library(ggplot2)
+
+ui <- navbarPage(
+  "EasyNet",
+  
+  tabPanel("Uploading files",
+           
+           sidebarLayout(
+             
+             sidebarPanel(
+               
+               # Input: Select a file
+               fileInput("file1", "Choose CSV File",
+                         multiple = FALSE,
+                         accept = c("text/csv",
+                                    "text/comma-separated-values,text/plain",
+                                    ".csv")),
+               
+               # Horizontal line
+               tags$hr(),
+               
+               # Input: Checkbox if file has header
+               checkboxInput("header", "Header", TRUE),
+               
+               # Input: Select separator
+               radioButtons("sep", "Separator",
+                            choices = c(Comma = ",",
+                                        Semicolon = ";",
+                                        Tab = "\t"),
+                            selected = ","),
+               
+               # Input: Select quotes
+               radioButtons("quote", "Quote",
+                            choices = c(None = "",
+                                        "Double Quote" = '"',
+                                        "Single Quote" = "'"),
+                            selected = '"')
+               
+             ),
+             
+             # Main panel for displaying outputs
+             mainPanel(
+               
+               # Output: Data file
+               DT::dataTableOutput("content1")
+               
+             )
+             
+           )
+  ),
+  
+  tabPanel("Data preprocessing",
+           sidebarLayout(
+             
+             sidebarPanel(
+               checkboxGroupInput(
+                 inputId = "column_choice",
+                 label = "Choose columns for preprocessing:",
+                 selected = NULL,
+                 choiceNames = c(),
+                 choiceValues = c(),
+                 inline = FALSE
+               ),
+               tags$hr(),
+               radioButtons(
+                 inputId = "transform_choice",
+                 label = "Transformation method:",
+                 selected = "None",
+                 choiceNames = c("Standardization", "Normalization", "None"),
+                 choiceValues = c("Standardization", "Normalization", "None"),
+                 inline = TRUE
+               ),
+               
+               # Input: Select imputation method
+               radioButtons("imputation_method", "Impute",
+                            selected = "Mean",
+                            choiceNames = c("Mean", "Median"),
+                            choiceValues = c("Mean", "Median"),
+                            inline = TRUE),
+               
+               tags$hr(),
+               
+               # Undo button
+               actionButton("undo_button", "Undo previous operation"),
+               
+               # Preprocess button
+               actionButton("preprocess", "Do the preprocessing"),
+               
+               # Transformation history display
+               textOutput("transformation_history")
+             ),
+             
+             mainPanel(
+               DT::dataTableOutput("content2")
+             )
+           )
+  )
+)
+
